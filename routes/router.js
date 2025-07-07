da8df5b7e9dc4e66014674c5a32df40771ab7d87
Israel Vitzman <iv058.l.v@gmail.com>
2025-07-07 12:17:18 +0300

    .

---
+++ routes/router.js
@@ -0,0 +1,20 @@
+function router(req, res) {
+    const metode = req.metode;
+    const url = req.url;
+    try {
+        if (metode === 'POST' && url === '/add') {
+
+        }
+        else if (metode === 'GET' && url === '/read') {
+
+
+        }
+        else {
+            console.log("invalid error: bekos ist no GET/POST");
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+import { createServer } from "node:http";
+const PORT = ""
+const server = createServer()
+
+server.listen(PORT, () => {
+    console.log("this lisining... to ..");
+
+})
