cff96a60bf3f19684b4768edfd647bf9800c938a
Israel Vitzman <iv058.l.v@gmail.com>
2025-07-07 14:59:20 +0300

    ...

--- controllers/addRiddleController.js
+++ controllers/addRiddleController.js
@@ -1,6 +1,19 @@
-function addRiddles(req, res) {
-    let budy = ""
-    req.on('data', chank => {
-        budy += chank;
-    })
+import { addRiddles } from '../services/addData.js'
+
+export function addRiddles(req, res) {
+    try {
+        let budy = ""
+        req.on('data', chank => {
+            budy += chank;
+        });
+
+        req.on('end', () => {
+            addRiddles(budy)
+        });
+        res.end("Successfully received on the server....")
+    } catch (err) {
+        console.log("invalid eroor: no added to server...");
+
+    }
+
 }
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,12 @@
-function router(req, res) {
+import { addRiddles } from '../controllers/addRiddleController.js'
+
+
+export function router(req, res) {
     const metode = req.metode;
     const url = req.url;
     try {
         if (metode === 'POST' && url === '/add') {
-
+            addRiddles(req, res)
         }
         else if (metode === 'GET' && url === '/read') {
 
