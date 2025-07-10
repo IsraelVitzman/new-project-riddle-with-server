7a90003719bcb2e1cde8c2086d9f2a5f336cc62d
Israel Vitzman <iv058.l.v@gmail.com>
2025-07-10 12:52:47 +0300

    ....

--- controllers/delateRiddle.js
+++ controllers/delateRiddle.js
@@ -4,6 +4,7 @@ export async function DelateRiddles(req, res) {
     try {
         const body = req.params.id
         await DelateData(body)
+        res.end("deleted on the server....");
     } catch (err) {
         console.log("invalid eroor: no delated server...", err);
 
